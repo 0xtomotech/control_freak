54168b27ea64e7e9238eb98bab4a726a57783836
0xtomotech <0xtomotech@gmail.com>
2024-03-19 11:10:23 +0100

    added api/create-room post request, updated models

--- music_control_project/api/models.py
+++ music_control_project/api/models.py
@@ -20,7 +20,8 @@ def generate_unique_code():
 
 class Room(models.Model):
     # id field automatically created
-    code = models.CharField(max_length=8, default="", unique=True)
+    code = models.CharField(
+        max_length=8, default=generate_unique_code, unique=True)
     host = models.CharField(max_length=60, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
