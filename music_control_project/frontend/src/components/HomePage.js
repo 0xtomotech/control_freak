7ac23382ea7406d8c6ed63c95ded6963d572a4b9
0xtomotech <0xtomotech@gmail.com>
2024-03-20 11:42:26 +0100

    Improved Homepage, added user session for roomn tracking, and auto redirect

--- music_control_project/frontend/src/components/HomePage.js
+++ music_control_project/frontend/src/components/HomePage.js
@@ -1,22 +1,60 @@
 import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
+import CreateRoomPageWrapper from "./CreateRoomPage";
 import Room from "./Room";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RoomWrapper from "./Room";
+import RoomJoinPageWrapper from "./RoomJoinPage";
+import { Grid, Button, ButtonGroup, Typography } from "@mui/material";
+import { BrowserRouter as Router, Routes, Route, Link, Navigate} from "react-router-dom";
+
 
 export default class HomePage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            roomCode: null,
+        };
+    }
+    
+    async componentDidMount() {
+        fetch('/api/user-in-room')
+        .then((response) => response.json())
+        .then((data) => {
+            this.setState({
+                roomCode: data.code,
+            })
+        });
+    }
+
+    renderHomepage() {
+        return (
+            <Grid container spacing={3}>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h3" component="h3">
+                        Control Freak
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <ButtonGroup disableElevation variant="contained">
+                        <Button color="primary" to="/join" component={Link}>Join a Room</Button>
+                        <Button color="secondary" to="/create" component={Link}>Create a Room</Button>
+                    </ButtonGroup>
+                </Grid>
+            </Grid>
+        );
     }
 
     render() {
         return (
             <Router>
               <Routes>
-                <Route path="/" element={<p>This is the home page</p>} />
-                <Route path="/join" element={<RoomJoinPage />} />
-                <Route path="/create" element={<CreateRoomPage />} />
-                <Route path="/room/:roomCode" element={<Room />} />
+                <Route path="/" element={
+                      this.state.roomCode ? (<Navigate to={`/room/${this.state.roomCode}`} replace />) : (this.renderHomepage())
+                    } />
+                <Route path="/join" element={<RoomJoinPageWrapper />} />
+                <Route path="/create" element={<CreateRoomPageWrapper />} />
+                <Route path="/room/:roomCode" element={<RoomWrapper />} />
               </Routes>
             </Router>
           );
