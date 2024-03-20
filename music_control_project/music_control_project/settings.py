cd985876dac155b613797de887d4605e0b02788d
0xtomotech <0xtomotech@gmail.com>
2024-03-20 10:40:14 +0100

    Updated variables

--- music_control_project/music_control_project/settings.py
+++ music_control_project/music_control_project/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7+x2+fzmgx1=vd3czsu5e06@47n1^ob(!9i2%xso!axu0$*g@c'
+# Important: Use the .env file to store the SECRET_KEY environme
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
